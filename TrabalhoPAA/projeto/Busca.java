ebd8f5a6eaec890b9ec1f3600f0d60a13d1f168a
Thomas Adam da Costa <thomas.costa@alunos.ifsuldeminas.edu.br>
2020-11-10 20:02:52 -0300

    Update Busca.java

--- TrabalhoPAA/projeto/Busca.java
+++ TrabalhoPAA/projeto/Busca.java
@@ -24,7 +24,7 @@ public class Busca {
     public static int buscaSequencial(int[] vetor, int n, int teste) {
         int posicao = -1; // valor padrao para não achar a posição (-1)
         // Algoritmo comeca aqui
-        //AuxiliarVetor.wait(1);// tempo de espera padrao
+        AuxiliarVetor.wait(1);// tempo de espera padrao
         for (int i = 0; i < n; i++) {
             posicao = -1; // garantia de valor padrao caso nao ache o elemento
             if (vetor[i] == teste) { // verifica se existe o elemento em cada posicao
